98d0e7bc2aea8ffc73294994f5033b73c6b1f8cd
Giuseppe Marcucci <marcucci.dev@gmail.com>
2021-04-19 20:42:24 +0200

    [API] adjusted parameters of get_images_urls()

--- pixabay_connection.py
+++ pixabay_connection.py
@@ -6,48 +6,45 @@ from config import api_key
 # https://pixabay.com/api/?key=XXX&q=yellow+flowers&image_type=photo&pretty=true
 
 
-def get_images_urls(number_images=20, query=None, images_per_page=20):  # , page=1, per_page=20):
-    request_string = "https://pixabay.com/api/?key="+api_key+"&q=yellow+flowers&image_type=photo&pretty=true"
-    request_string += "&per_page="+str(images_per_page)
-    print(request_string)
+def get_images_urls(number_images=20, query='yellow+flowers'):
+
+    if number_images >= 200:
+        images_per_page = 200
+    elif number_images >= 3:  # min is 3
+        images_per_page = number_images
+    else:
+        return []
+    page = 1  # at the moment
+
+    request_string = "https://pixabay.com/api/?key={}".format(api_key)
+    request_string += "&q={}".format(query)
+    request_string += "&image_type=photo"
+    request_string += "&pretty=true"
+    request_string += "&page={}".format(page)
+    request_string += "&per_page={}".format(images_per_page)
+
     r = requests.get(request_string)
-    print(r)
-    # print(r.json())
     json_response = r.json()
     print("r.status_code", r.status_code)
-    print("r.status_code type", type(r.status_code))
-    
+
     images_urls = []
     if r.status_code == 200:  # 200: OK
-        print("OK")
-        
-        for i in range(images_per_page):# range(json_response["totalHits"]):
-            print("i: ", i)
-            print(json_response["hits"][i]["tags"])
-            
-            images_urls.append(json_response["hits"][i]["largeImageURL"])
-            
-        # print(r.text)
-        # print(r.json())
-        #print(r.json()["total"])
-        #print(r.json()["totalHits"])
-        #print(r.json()["hits"])
-        #print(r.json()["hits"][0])  # dizionario!
-        #print(r.json()["hits"][0]["pageURL"])
-        ## largeImageURL or webformatURL
-        #print(r.json()["hits"][0]["largeImageURL"])
-        
+
+        for index, item in enumerate(json_response["hits"]):
+            print("index:", index)
+            print("item:",  item)
+            images_urls.append(json_response["hits"][index]["largeImageURL"])
+
+        # print(r.json()["total"])
+        # print(r.json()["totalHits"])
+        # print("TOT :", print(r.json()["totalHits"]))
     return images_urls
 
-# "total": 4692,
-#	"totalHits": 500,
-#	"hits": [
 
 def main():
 
-    print("Result: ", get_images_urls(images_per_page=30))
+    print("Result: ", get_images_urls(number_images=30))
     
-    
-    
+
 if __name__ == "__main__":
     main()
