2b494cf03e3585f9728d7c15bec2dedc618bd22a
Giuseppe Marcucci <marcucci.dev@gmail.com>
2021-04-19 10:54:31 +0200

    Get the urls of the images

---
+++ pixabay_connection.py
@@ -0,0 +1,52 @@
+import requests
+# from imgurpython import ImgurClient
+
+
+# example:
+# https://pixabay.com/api/?key=XXX&q=yellow+flowers&image_type=photo&pretty=true
+
+# get key from my local txt file
+client_key = 'XXX' Get your key after registering on pixabay.com
+
+
+
+def get_images_urls(number_images=20, query=None, images_per_page=20):  # , page=1, per_page=20):
+    # request_string 
+    request_string = "https://pixabay.com/api/?key="+client_key+"&q=yellow+flowers&image_type=photo&pretty=true"
+    request_string += "&per_page="+str(images_per_page)
+    print(request_string)
+    r = requests.get(request_string)
+    print(r)
+    # print(r.json())
+    json_response = r.json()
+    print("r.status_code", r.status_code)
+    print("r.status_code type", type(r.status_code))
+    
+    images_urls = []
+    if r.status_code == 200:  # 200: OK
+        print("OK")
+        
+        for i in range(images_per_page):# range(json_response["totalHits"]):
+            print("i: ", i)
+            print(json_response["hits"][i]["tags"])
+            
+            images_urls.append(json_response["hits"][i]["largeImageURL"])
+            
+        # print(r.text)
+        # print(r.json())
+        #print(r.json()["total"])
+        #print(r.json()["totalHits"])
+        #print(r.json()["hits"])
+        #print(r.json()["hits"][0])  # dizionario!
+        #print(r.json()["hits"][0]["pageURL"])
+        ## largeImageURL or webformatURL
+        #print(r.json()["hits"][0]["largeImageURL"])
+        
+    return images_urls
+
+#"total": 4692,
+#	"totalHits": 500,
+#	"hits": [
+
+
+print("Result: ", get_images_urls(images_per_page=30))
