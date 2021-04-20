06b53139bdd34efd56c42a19f5af5305f935e939
Lorenzo Muzzi <lorenzomuzzi28@gmail.com>
2021-04-20 23:50:50 +0200

    add downloads folder

--- main.py
+++ main.py
@@ -22,18 +22,17 @@ def main():
         if i <= len(key_words) - 2:
             query += "+"
 
-    # Name the folder where saving the images
-    folder = input("Insert the name of the folder where you want to save images: ")
-
     # Choose the max number of threads at work
     workers = int(input("How many threads are working? "))
     if workers > 200:
         workers = 200
         print("Impossible to select a value higher than 200")
 
+    # Save the images in downloads folder
+    folder = "downloads"
+    
     # Call thread_get_images and start downloading
-    local_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), folder)
-    thread_get_images(number_images, query, local_path, workers)
+    thread_get_images(number_images, query, folder, workers)
 
 # Call the main function
 if __name__ == "__main__":
