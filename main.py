51f62a205b60df36fd2a98433ab2ab0c0a6b0cae
Lorenzo Muzzi <lorenzomuzzi28@gmail.com>
2021-04-20 18:25:32 +0200

    renaming max_worker variable

--- main.py
+++ main.py
@@ -13,14 +13,14 @@ def main():
     folder = input("Insert the name of the folder where you want to save images: ")
 
     # Choose the max number of threads at work
-    max_worker = int(input("How many threads are working? "))
-    if max_worker > 200:
-        max_worker = 200
+    workers = int(input("How many threads are working? "))
+    if workers > 200:
+        workers = 200
         print("Impossible to select a value higher than 200")
 
     # Call thread_get_images and start downloading
     local_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), folder)
-    thread_get_images(number_images, query, local_path, max_worker)
+    thread_get_images(number_images, query, local_path, workers)
 
 # Call the main function
 if __name__ == "__main__":
