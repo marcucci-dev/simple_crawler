66c34a1a049d968f40810697ddfbe63110149ab0
Lorenzo Muzzi <lorenzomuzzi28@gmail.com>
2021-04-20 16:28:56 +0200

    store all inputs in main()

--- main.py
+++ main.py
@@ -2,19 +2,23 @@ import os
 import threads_download
 from threads_download import thread_get_images
 
-# Choose the number of images to download
-number_images = int(input("How many images you want to download? "))
+def main():
+    # Choose the number of images to download
+    number_images = int(input("How many images you want to download? "))
 
-# Choose the content of the images
-query = input("What's the subject of your images? ")
+    # Choose the content of the images
+    query = input("What's the subject of your images? ")
 
-# Name the folder where saving the images
-folder = input("Insert the name of the folder where you want to save images: ")
+    # Name the folder where saving the images
+    folder = input("Insert the name of the folder where you want to save images: ")
 
-# Choose the max number of threads at work
-max_workers = int(input("How many threads are working? "))
+    # Choose the max number of threads at work
+    max_workers = int(input("How many threads are working? "))
 
-# Call thread_get_images and start downloading
-local_path = os.path.abspath("/" + folder)
-thread_get_images(number_images, query, local_path, max_workers)
+    # Call thread_get_images and start downloading
+    local_path = os.path.abspath("/" + folder)
+    thread_get_images(number_images, query, local_path, max_workers)
 
+# Call the main function
+if __name__ == "__main__":
+    main()
